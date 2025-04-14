64efbe1ec151378a8802c1205ccb16ff7da95165
vadzimavich <vadzimavich@gmail.com>
2025-04-15 02:00:29 +0300

    fix: queuePromises misspell

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -139,7 +139,7 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(promises) {
+function queuePromises(promises) {
   return promises.reduce((acc, promise) => {
     return acc.then((result) => {
       return promise.then((value) => {
@@ -156,5 +156,5 @@ module.exports = {
   getFirstPromiseResult,
   getAllOrNothing,
   getAllResult,
-  queuPromises,
+  queuePromises,
 };
